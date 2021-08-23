57bad41e67df46d766b10200c0f57775830f6858
YiQiangWu <wu_yiqiang@outlook.com>
2021-08-23 21:10:08 +0800

    添加后台管理系统

---
+++ Vue3-Element-Admin/src/views/home/index.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Date: 2021-04-20 11:06:21
+ * @LastEditors: huzhushan@126.com
+ * @LastEditTime: 2021-04-23 15:16:12
+ * @Author: huzhushan@126.com
+ * @HomePage: https://huzhushan.gitee.io/vue3-element-admin
+ * @Github: https://github.com/huzhushan/vue3-element-admin
+ * @Donate: https://huzhushan.gitee.io/vue3-element-admin/donate/
+-->
+<template>
+  <div class="home">home</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'home',
+  setup() {},
+})
+</script>
+
+<style lang="scss" scoped>
+.home {
+  color: $mainColor;
+}
+</style>
